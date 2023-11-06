38816194a02d8771e3f394b7907e0b4de022ed40
Colin <colinclissonhg@gmail.com>
2023-11-06 15:59:45 -0500

    feat: refactor navbar in its own component

---
+++ src/components/TopNavBar.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+    import { search } from "$lib/store/index.js";
+    import { Button, ButtonGroup, CloseButton, Input, Navbar, NavBrand } from "flowbite-svelte";
+    import { ArrowRightSolid, ArrowUpSolid, ChevronDownSolid, SearchOutline } from "flowbite-svelte-icons";
+
+    let isSearchEnabled = false;
+
+    const onSearchChange = (event: Event) => search.set((event.target as HTMLInputElement).value);
+    const handleEnterKeyPress = (event: KeyboardEvent) => {
+        if (event.key !== "Enter") return;
+        isSearchEnabled = false;
+    };
+</script>
+
+<Navbar class="dark bg-blue-950">
+    {#if !isSearchEnabled}
+        <div class="w-full flex items-center justify-between">
+            <NavBrand href="/">
+                <span class="text-white font-black text-3xl flex items-center">
+                    <span>FLE</span>
+                    <span class="text-primary-600">X</span>
+                </span>
+            </NavBrand>
+            <div class="hidden md:block flex-1 max-w-3xl mx-3">
+                <Input
+                    value={$search}
+                    on:input={onSearchChange}
+                    placeholder="Search movie..."
+                    class="!bg-white/10 hover:!bg-white/20 focus:!bg-white/20"
+                >
+                    <SearchOutline
+                        slot="left"
+                        class="w-4 h-4"
+                    />
+                </Input>
+            </div>
+            <div class="flex items-center gap-3">
+                <Button
+                    on:click={() => isSearchEnabled = true}
+                    outline
+                    class="md:hidden border-0 !bg-transparent !p-2"
+                >
+                    <SearchOutline class="outline-0 text-white" />
+                </Button>
+                <Button>Upload</Button>
+            </div>
+        </div>
+    {:else}
+        <ButtonGroup class="w-full">
+            <Input
+                value={$search}
+                on:input={onSearchChange}
+                on:keypress={handleEnterKeyPress}
+                placeholder="Search a movie..."
+                type="search"
+                autofocus
+            >
+                <CloseButton
+                    on:click={() => search.set("")}
+                    slot="right"
+                    class={`!ring-0 ${$search.length === 0 ? "hidden" : ""}`}
+                />
+            </Input>
+            <Button
+                on:click={() => isSearchEnabled = false}
+                color="primary"
+            >
+                <ArrowRightSolid />
+            </Button>
+        </ButtonGroup>
+    {/if}
+</Navbar>
