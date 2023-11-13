8fdd04ce42ef93702e8987c7066d6f0a95fae6e2
Colin <colinclissonhg@gmail.com>
2023-11-13 15:29:28 -0500

    feat: add tmdb movie parser

--- src/lib/parsers/movie.ts
+++ src/lib/parsers/movie.ts
@@ -1,5 +1,5 @@
 import type { drive_v3 } from "googleapis";
-import type { FileAppProperties, Movie } from "../../types/movie";
+import type { FileAppProperties, Movie, TmdbMovie } from "@/types/movie";
 
 export const parseFileIntoMovie = (file: drive_v3.Schema$File): Movie => {
     const appProperties = file.appProperties as unknown as FileAppProperties;
@@ -16,4 +16,30 @@ export const parseFileIntoMovie = (file: drive_v3.Schema$File): Movie => {
         releaseDate: appProperties.release_date,
         uploadDate: appProperties.upload_date,
     };
+};
+
+export const parseTmdbMovie = (data: {
+    id: string
+    imdb_id: string
+    title: string
+    overview: string
+    poster_path: string
+    backdrop_path: string
+    vote_average: number
+    runtime: number
+    release_date: string
+}) => {
+    const movie: TmdbMovie = {
+        tmdbId: data.id,
+        imdbId: data.imdb_id,
+        name: data.title,
+        description: data.overview,
+        image: data.poster_path,
+        backdrop: data.backdrop_path,
+        rating: +(data.vote_average / 2).toFixed(2),
+        duration: data.runtime,
+        releaseDate: data.release_date,
+    };
+
+    return movie;
 };
