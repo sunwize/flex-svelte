bfa295d5ab126ca17b656727b0225c7032728797
Colin <colinclissonhg@gmail.com>
2023-11-16 00:11:47 -0500

    chore: improve movie page

--- src/routes/movies/[id]/+page.svelte
+++ src/routes/movies/[id]/+page.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
-    import { page } from "$app/stores";
     import { onMount } from "svelte";
-    import { api } from "$lib/services/api";
-    import type { Movie, TmdbMovie } from "@/types/movie";
-    import { tmdbPosterHD } from "$lib/images/poster";
-    import { Button, Rating, Skeleton, TextPlaceholder, VideoPlaceholder } from "flowbite-svelte";
+    import { format, intervalToDuration } from "date-fns";
+    import { Badge, Button, Rating, Skeleton, TextPlaceholder, VideoPlaceholder } from "flowbite-svelte";
     import { PlaySolid } from "flowbite-svelte-icons";
+
+    import type { Movie, TmdbMovie } from "@/types/movie";
+    import { page } from "$app/stores";
+    import { api } from "$lib/services/api";
+    import { tmdbPosterHD } from "$lib/images/poster";
     import { movie, visible } from "$lib/store/player";
-    import { addSeconds, format } from "date-fns";
 
     let tmdbMovie: TmdbMovie;
     let loading = false;
+
     $: loaded = tmdbMovie && !loading;
+    $: duration = getDuration(tmdbMovie);
+
+    const getDuration = (tmdbMovie: TmdbMovie) => {
+        if (!tmdbMovie) return "";
+        const duration = intervalToDuration({ start: 0, end: tmdbMovie?.duration * 60 * 1000 });
+        return `${duration.hours}h ${duration.minutes}min`;
+    };
 
     onMount(async () => {
         try {
@@ -27,7 +36,7 @@
     });
 </script>
 
-<section class="relative flex flex-col w-full flex-1 leading-snug max-w-6xl mx-auto md:py-10 md:px-1">
+<section class="dark relative flex flex-col w-full flex-1 leading-snug max-w-6xl mx-auto md:py-10 md:px-1">
     <article class="hidden md:grid grid-cols-12 w-full flex-1">
         {#if loaded}
             <img
@@ -43,16 +52,11 @@
         <div class="col-span-7 bg-white/10 rounded-l-md md:rounded-l-none rounded-r-md p-5">
             {#if loaded}
                 <h1 class="text-4xl font-bold mb-1">{tmdbMovie.name}</h1>
-                <p class="opacity-70 mb-5">
-                    {format(new Date(tmdbMovie.releaseDate), "yyyy")}
-                    ·
-                    {format(addSeconds(new Date(), tmdbMovie.duration), "h")}h{format(addSeconds(new Date(), tmdbMovie.duration), "mm")}
-                </p>
 
                 <Rating
                     total={5}
                     rating={tmdbMovie.rating}
-                    class="-ml-1.5 mb-2"
+                    class="-ml-1.5 mb-5"
                 />
                 <div class="mb-5">
                     <Button
@@ -62,6 +66,23 @@
                         <span class="mr-3">Lire</span>
                         <PlaySolid />
                     </Button>
+                </div>
+
+                <div class="mb-2">
+                    <Badge
+                        color="dark"
+                        border
+                        large
+                    >
+                        {format(new Date(tmdbMovie.releaseDate), "yyyy")}
+                    </Badge>
+                    <Badge
+                        color="dark"
+                        border
+                        large
+                    >
+                        {duration}
+                    </Badge>
                 </div>
 
                 <p>
